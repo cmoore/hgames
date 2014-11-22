4f25cb84533220cc8afee2680f2b44b460fd3153
Clint Moore <clint@ivy.io>
2014-11-22 14:13:56 -0800

    Ugh.

--- src/io/ivy/hgames/HGamesListener.java
+++ src/io/ivy/hgames/HGamesListener.java
@@ -11,14 +11,13 @@ import net.canarymod.api.inventory.Item;
 import net.canarymod.api.inventory.ItemType;
 import net.canarymod.api.world.Chunk;
 import net.canarymod.api.world.World;
-import net.canarymod.api.world.blocks.BlockType;
-import net.canarymod.api.world.blocks.Chest;
-import net.canarymod.api.world.blocks.Sign;
-import net.canarymod.api.world.blocks.TileEntity;
+import net.canarymod.api.world.blocks.*;
+import net.canarymod.api.world.blocks.properties.*;
 import net.canarymod.api.world.position.Position;
 import net.canarymod.hook.HookHandler;
 import net.canarymod.hook.player.BlockRightClickHook;
 import net.canarymod.plugin.PluginListener;
+import net.canarymod.api.entity.living.humanoid.Player;
 
 public class HGamesListener implements PluginListener {
 	
@@ -28,41 +27,65 @@ public class HGamesListener implements PluginListener {
 	public void onBlockRightClickHook(BlockRightClickHook hook) {
 		if (hook.getPlayer().getName().equals("hydo")) {
 			if (hook.getPlayer().getWorld().getFqName().equals("hgames_NORMAL")) {
-				if (hook.getBlockClicked().getType().equals(BlockType.SignPost)) {
-					Sign sign = (Sign) hook.getBlockClicked();
-					if (sign.getTextOnLine(0).equals("rescan")) {
-						scan_for_chests(hook.getPlayer().getWorld());
+          Block block = hook.getBlockClicked();
+          if (block.getType().equals(BlockType.SignPost)) {
+              Sign sign = (Sign) block.getTileEntity();
+              if (sign.getTextOnLine(0).equals("rescan")) {
+                  scan_for_chests(hook.getPlayer().getWorld(), hook.getPlayer());
 				
-						ItemFactory factory = Canary.factory().getItemFactory();
-						chests.forEach(new Consumer<Chest>() {
-							@Override
-							public void accept(Chest chest) {
-								chest.addItem(factory.newItem(ItemType.IronSword));
-							}
-						});
-					}
-				}
+                  /*
+                  ItemFactory factory = Canary.factory().getItemFactory();
+                  chests.forEach(new Consumer<Chest>() {
+                          @Override
+                          public void accept(Chest chest) {
+                              chest.clearInventory();
+                              chest.addItem(factory.newItem(ItemType.IronSword));
+                              chest.update();
+                          }
+                      });
+                  */
+              }
+          }
 			}
 		}
 	}
 	
-	private void scan_for_chests(World world) {
-		world.getLoadedChunks().forEach(new Consumer<Chunk>() {
-			@Override
-			public void accept(Chunk chunk) {
-				chunk.getTileEntityMap().forEach(new BiConsumer<Position,TileEntity>() {
-					@Override
-					public void accept(Position position, TileEntity tileentity) {
-						if (tileentity.getBlock().getType().equals(BlockType.Chest)) {
-							Chest chest = (Chest) tileentity.getBlock().getTileEntity();
-							if (!chests.contains(chest)) {
-								chests.add(chest);
-							}
-						}
-					}
-				});
-			}
-		});
+    private void scan_for_chests(World world, Player me) {
+      List<Chunk> chunks = world.getLoadedChunks();
+      me.notice("Chunks: ".concat(String.valueOf(chunks.size())));
+      chunks.forEach(new Consumer<Chunk>() {
+              @Override
+              public void accept(Chunk chunk) {
+                  if (chunk.getTileEntityMap().size() > 0) {
+                      chunk.getTileEntityMap().forEach(new BiConsumer<Position,TileEntity>() {
+                              @Override
+                              public void accept(Position position, TileEntity tileentity) {
+                                  if (tileentity.getBlock().getType().equals(BlockType.Chest)) {
+                                      Chest chest = (Chest) tileentity;
+                                      chests.add(chest);
+                                  }
+                              }
+                          });
+                  }
+              }
+          });
+      
+      // chunks.forEach(new Consumer<Chunk>() {
+      //         @Override
+      //         public void accept(Chunk chunk) {
+      //             chunk.getTileEntityMap().forEach(new BiConsumer<Position,TileEntity>() {
+      //                     @Override
+      //                     public void accept(Position position, TileEntity tileentity) {
+      //                         if (tileentity.getBlock().getType().equals(BlockType.Chest)) {
+      //                             Chest chest = (Chest) tileentity;
+      //                             if (!chests.contains(chest)) {
+      //                                 chests.add(chest);
+      //                             }
+      //                         }
+      //                     }
+      //                 });
+      //         }
+      //     });
 	}
 }
 
